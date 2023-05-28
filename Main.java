45ed6370b642df3bceba88cd1ee4e4a05df9317a
isaapereiraa <113491106+isaapereiraa@users.noreply.github.com>
2023-05-28 17:10:37 -0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Clases;
+
+import Interfaces.TXT;
+
+/**
+ *
+ * @author Isabella Pereira, Adrian Lovera, Alejandra Carrera
+ */
+public class Main {
+
+    /**
+     * @param args 
+     */
+    public static void main(String[] args) {
+       TXT primeraInterfaz = new TXT();
+       primeraInterfaz.setVisible(true);
+    }
+    
+}
